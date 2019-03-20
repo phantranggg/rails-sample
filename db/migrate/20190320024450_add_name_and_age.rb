b3db3ddc9b9cacceb54b9bc743cf449b934d1b12
Trang Phan <phantrang3006@gmail.com>
2019-03-20 02:50:00 +0000

    Ruby on Rails(2)

---
+++ db/migrate/20190320024450_add_name_and_age.rb
@@ -0,0 +1,7 @@
+class AddNameAndAge < ActiveRecord::Migration
+  def change
+    add_column :users, :name, :string
+    add_column :users, :age, :integer
+    add_index :users, :name, unique: true
+  end
+end
